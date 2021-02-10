b8edfd9fdb90b65511b62268b0aaa3f1f82e27df
ClintCrab <clint.crabtree@hotmail.com>
2021-02-09 17:39:13 -0800

    added a line return in app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
